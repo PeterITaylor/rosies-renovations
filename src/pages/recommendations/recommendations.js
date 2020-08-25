6aa121ab76b46e9116bd999829bf754a7318144e
Danial Taylor <danial@graphventure.com>
2020-08-25 17:48:18 -0500

    added layout to home page

---
+++ src/pages/recommendations/recommendations.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './recommendations.css'
+
+export const Recommendations = ({children}) => {
+    return (
+        <div className='recommendations'>
+            Recommendations
+        </div>
+    )
+}
